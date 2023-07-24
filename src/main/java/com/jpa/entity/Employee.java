4bdbee2826124b92a1520b56da91cd46b9bf887e
soumi487 <soumi.roy487@gmail.com>
2023-07-24 20:15:06 +0530

    first commit

--- src/main/java/com/jpa/entity/Employee.java
+++ src/main/java/com/jpa/entity/Employee.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name="oracletest")
 @Data
 @Entity
 public class Employee {
